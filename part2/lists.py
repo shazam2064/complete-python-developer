4d1953db50c53b952c1ad78ea4572e824377972b
shazam2064 <gabriel.salomon.2007@gmail.com>
2021-05-23 14:01:22 -0400

    Finished Part 1.

---
+++ part2/lists.py
@@ -0,0 +1,67 @@
+# my_list = [1, 2, 3, 4, 5]
+#
+# print(type(my_list))
+
+# my_list = [1, 2, 3, 4, 5]
+#
+# my_list.pop()
+#
+# print(my_list)
+
+# my_list = [1, 2, 3, 4, 5]
+#
+# my_list[0] = ['Ari Ari Ari', 'Arrivederci']
+#
+# print(my_list)
+
+# my_list = [1, 2, 3, 4, 5]
+#
+# my_list.append("This is requiem")
+#
+# print(my_list)
+
+# my_list = [1, 2, 3, 4, 5]
+#
+# my_list.append("This is requiem")
+#
+# my_list.pop()
+#
+# print(my_list)
+
+# my_list = [1, 2, 3, 4, 5]
+#
+# my_list.append("This is requiem")
+#
+# sentence = my_list.pop()
+#
+# print(my_list)
+# print(sentence)
+
+# my_list = [1, 2, 3, 4, 5]
+#
+# appended = my_list.append("This is requiem")
+#
+# sentence = my_list.pop()
+#
+# print(my_list)
+# print(sentence)
+# print(appended)
+
+# my_list = [4, 5, 3, 1, 2]
+#
+# my_list.sort()
+#
+# print(my_list)
+
+# my_list = [4, 5, 3, 1, 2]
+#
+# my_list.reverse()
+#
+# print(my_list)
+
+my_list = [4, 5, 3, 1, 2]
+
+my_list.sort()
+my_list.reverse()
+
+print(my_list)
